7f98e68f28ad5a3cefed174b4a4ce13953f4006d
Veronica <veronicapc1992@gmail.com>
2020-03-26 10:57:16 +0100

    adding sorting feature

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,38 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
+import React from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
 
-const Pagination = (props) => {
-    const { moviesCount, pageSize, currentPage, onPageChange } = props;
+const Pagination = ({ moviesCount, pageSize, currentPage, onPageChange }) => {
+  const pagesCount = Math.ceil(moviesCount / pageSize);
+  if (pagesCount === 1) return null;
 
-    const pagesCount = Math.ceil(moviesCount / pageSize);
-    if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
 
-    const pages = _.range(1, pagesCount + 1);
-    
-    return ( 
-        <nav aria-label="...">
-            <ul className="pagination">
-                {pages.map(page =>
-                <li 
-                    key={page}
-                    className={ page === currentPage ? 'page-item active' : 'page-item'}>
-                    <a
-                        className="page-link"
-                        onClick={() => onPageChange(page)}>{page}
-                    </a>
-                </li>
-                )}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav aria-label="...">
+      <ul className="pagination">
+        {pages.map(page => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <span className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
- 
+
 Pagination.propTypes = {
-    moviesCount: PropTypes.number.isRequired, 
-    pageSize: PropTypes.number.isRequired,
-    currentPage: PropTypes.number.isRequired,
-    onPageChange: PropTypes.func.isRequired
+  moviesCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default Pagination;
