6a32ad37aa123dd8722faf6d353b5b9ae1b1b554
Veronica <veronicapc1992@gmail.com>
2020-03-30 09:20:18 +0200

    uploading new page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React, {Component} from 'react';
-import Movies from './components/movies';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navBar";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import MovieForm from "./components/movieForm";
+import NotFound from "./components/common/notFound";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import "./App.css";
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <main className="container">
-        <Movies />
-      </main>
-     );
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/movies/:id" component={MovieForm} />} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
   }
 }
- 
+
 export default App;
